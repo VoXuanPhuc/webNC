2a45377b56ce0d0fa9113dda17cbcce12a4f7153
Thuthuy1311 <72400802+Thuthuy1311@users.noreply.github.com>
2021-12-07 14:33:27 +0700

    Thuy sua header

--- Models/connection.php
+++ Models/connection.php
@@ -7,7 +7,7 @@
             //Thong so ket noi CSDL
             $severname ="localhost"; 
             $username ="root";
-            $password ="admin1234"; 
+            $password =""; 
             $db_name ="baeshop";
  
             //Tao ket noi CSDL
