6805e49a481ea801efb39a23725fb771f73c006b
PhanQuocDat-2102 <datphan2102@gmail.com>
2022-01-12 20:28:24 +0700

    update fix bug

--- Views/login/my-info.php
+++ Views/login/my-info.php
@@ -8,7 +8,7 @@
             </div>
         </div>
         <div class="row" style="padding-left: 16px;">
-            <div class="col-2 container-info-navbar" >
+            <div class="col-2 container-info-navbar">
                 <div class="content-navbar">
                     <div class="header-navbar">
                         <div class="header-navbar-img">
